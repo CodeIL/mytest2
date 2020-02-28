a02573aad7cc1a77d74b7c58bd8a9749cc7ebeb7
itsHing <koihingchu@yahoo.com>
2020-02-28 14:21:10 +0800

    branch1

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -7,5 +7,7 @@ public class Main {
         System.out.println("HelloWorld!");
         System.out.println("Hello");
         System.out.println("World!");
+        System.out.println("branch1");
+
     }
 }
